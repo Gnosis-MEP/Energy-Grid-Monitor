5d3a5b98e5639f9e8c97ce355a2074a58870223d
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2020-06-21 16:17:36 +0100

    start up scrtipt

--- execute.sh
+++ execute.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+pipenv run python webserver/app.py &
+echo 'webserver runnin'
+cd src
 sudo python3 cleanup_GPIO.py
-sudo mv energenie.csv energenie.csv.old
 sudo python3 monitor.py > out.log 2>&1 &
+cd ../
